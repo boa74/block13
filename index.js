9e163676d11a9be75f5c24f470695e3a85363818
boa74 <boa8574.kim@gmail.com>
2024-04-17 17:32:52 -0400

    First commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+
+return ((fahren-32)*5/9)
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +25,28 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  if(fahren<32){
+     return "very cold"
+  }else if(32<=fahren&&fahren<64){
+     return "cold"
+  }else if(64<=fahren&&fahren<86){ 
+     return "warm"
+  }else if(86<=fahren&&fahren<100){
+     return "hot"
+  }
+  }
+
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+
+  return Math.floor(Math.random() * 11);
+}
 
 // -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
 
